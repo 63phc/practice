633b984d3afe9b7bab2b6493e82cf95ffb5bc0f7
63phc <pavel.burns@gmail.com>
2018-04-27 11:47:53 +0500

    Next solved

---
+++ py.checkio.org/electronic-station/hamming-distance2.py
@@ -0,0 +1,8 @@
+"""
+You are given two positive numbers (N, M) in decimal form.
+You should calculate the Hamming distance between these two numbers in binary form.
+
+Input: Two arguments as integers.
+
+Output: The Hamming distance as an integer.
+"""
