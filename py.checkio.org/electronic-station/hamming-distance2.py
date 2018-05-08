ae361b65e5bb821b7492a65ec81a809244c3f012
63phc <pavel.burns@gmail.com>
2018-05-08 09:53:48 +0500

    Next solved

--- py.checkio.org/electronic-station/hamming-distance2.py
+++ py.checkio.org/electronic-station/hamming-distance2.py
@@ -6,3 +6,12 @@ Input: Two arguments as integers.
 
 Output: The Hamming distance as an integer.
 """
+
+def checkio(n, m):
+    return sum(int(c) for c in format(n^m, "b"))
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(117, 17) == 3, "First example"
+    assert checkio(1, 2) == 2, "Second example"
+    assert checkio(16, 15) == 5, "Third example"
