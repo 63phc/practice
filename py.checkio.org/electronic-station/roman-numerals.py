2ee1222eaaa202f2565ed12e68d73381da4452e3
63phc <pavel.burns@gmail.com>
2018-05-21 10:21:40 +0500

    next solut

--- py.checkio.org/electronic-station/roman-numerals.py
+++ py.checkio.org/electronic-station/roman-numerals.py
@@ -4,3 +4,31 @@ Input: A number as an integer.
 Output: The Roman numeral as a string.
 
 """
+ROMANS = (('M',  1000),
+          ('CM', 900),
+          ('D',  500),
+          ('CD', 400),
+          ('C',  100),
+          ('XC', 90),
+          ('L',  50),
+          ('XL', 40),
+          ('X',  10),
+          ('IX', 9),
+          ('V',  5),
+          ('IV', 4),
+          ('I',  1))
+
+def checkio(data):
+    roman_numeral = ""
+    for roman, value in ROMANS:
+        while data >= value:
+            data -= value
+            roman_numeral += roman
+    return roman_numeral
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
